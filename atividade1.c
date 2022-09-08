e3dc233781e7d7b642532efd2bf4282c400c3204
Manuelly Rodrigues Victor <manuellyvictor2000@gmail.com>
2022-09-08 09:46:42 -0300

    alterando

--- atividade1.c
+++ atividade1.c
@@ -17,17 +17,23 @@ int dataValida (int dia, int mes, int ano) {
    if (ano < 0 || mes < 1 || mes > 12 || dia < 1) {
     return 0;
    }
+
    if (mes == 2) {
     if (bissexto(ano)) {
         maiorDia = 29;
-    } else {
+    } 
+    else {
         maiorDia = 28;
     }
-   } else if (mes == 4 || mes == 6 || mes == 0 || mes == 11) {
+   }
+
+   else if (mes == 4 || mes == 6 || mes == 0 || mes == 11) {
     maiorDia = 30;
-   } else {
+   } 
+   else {
     maiorDia = 31;
    } 
+
    if (dia > maiorDia) {
     return 0;
    }
@@ -37,7 +43,8 @@ int dataValida (int dia, int mes, int ano) {
 int bissexto(int ano) {
     if ((ano % 4 == 0) && (ano % 100 != 0)) {
         return 1;
-    } else if (ano % 400 == 0) {
+    } 
+    else if (ano % 400 == 0) {
         return 1;
     } else {
         return 0;
