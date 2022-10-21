5c581bd30263b34b4ddf0bf6a75cf6911124bacf
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2022-10-20 21:08:27 -0300

    teste para o projeto

--- structPonteiro2.c
+++ structPonteiro2.c
@@ -13,12 +13,9 @@ int main()
 
     struct item item1, *ptr_item1;
 
-    //leitura de dados do usuário. Não trabalhar diretamente com a variável usuario1, mas sim com o endereço dela
     ptr_item1 = &item1;
     printf("\n Nome: ");
-    //scanf("%s", &usuario1); - assim fica errado, pois só está passando o endereço da struct e não do campo
-    //precisamos que o scanf passe o endereço do campo dentro da struct, logo, o certo é:
-    scanf("%s", ptr_item1-> nomeItem); //usa -> para acessar um endereço dentro de ptr_usuario1
+    scanf("%s", ptr_item1-> nomeItem);
     printf("\n Nome da marca: ");
     scanf("%s", ptr_item1-> nomeMarca);
     printf("\n Código de barras: ");
@@ -26,11 +23,11 @@ int main()
     printf("\n Data de validade: ");
     scanf("%s", ptr_item1-> dataValidade);    
 
-    //exibir dados coletados
-    printf("--------------- CADASTRO DO USUÁRIO! ---------------");
-    printf("Nome do usuário: %s", ptr_usuario1 -> nome); //precisa passar para o printf a variável que ficará no lugar do %s
-    printf("Email do usuário: %s", ptr_usuario1 -> email);
-    printf("Data de nascimento do usuário: %s", ptr_usuario1 -> dataNascimento);
+    printf("--------------- CADASTRO DE ITENS! ---------------");
+    printf("Nome do item: %s", ptr_item1 -> nomeItem); 
+    printf("Nome da marca: %s", ptr_item1 -> nomeMarca);
+    printf("Código de barras do item: %s", ptr_item1 -> codigoBarras);
+    printf("Data de validade do item: %s", ptr_item1 -> dataValidade);  
     
     return 0;
 
