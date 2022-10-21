b16c6ce20b35c6026cb7706ea942085fad1db288
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2022-10-20 21:05:38 -0300

    atualizando

--- structPonteiro2.c
+++ structPonteiro2.c
@@ -18,11 +18,13 @@ int main()
     printf("\n Nome: ");
     //scanf("%s", &usuario1); - assim fica errado, pois só está passando o endereço da struct e não do campo
     //precisamos que o scanf passe o endereço do campo dentro da struct, logo, o certo é:
-    scanf("%s", ptr_usuario1-> nome); //usa -> para acessar um endereço dentro de ptr_usuario1
-    printf("\n Email: ");
-    scanf("%s", ptr_usuario1-> email);
-    printf("\n Data de nascimento: ");
-    scanf("%s", ptr_usuario1-> dataNascimento);    
+    scanf("%s", ptr_item1-> nomeItem); //usa -> para acessar um endereço dentro de ptr_usuario1
+    printf("\n Nome da marca: ");
+    scanf("%s", ptr_item1-> nomeMarca);
+    printf("\n Código de barras: ");
+    scanf("%s", ptr_item1-> codigoBarras);
+    printf("\n Data de validade: ");
+    scanf("%s", ptr_item1-> dataValidade);    
 
     //exibir dados coletados
     printf("--------------- CADASTRO DO USUÁRIO! ---------------");
